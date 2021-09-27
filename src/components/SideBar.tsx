dd636786e142f855b28a397f7859153e1e7ea4f5
gabiayako <gabiayako@gmail.com>
2021-09-27 07:31:27 -0300

    Implementing SideBar and Content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { GenreResponseProps } from '../App';
+import { api } from '../services/api';
+
+import { Button } from './Button';
+
+type SideBarProps = {
+  onClickButton: (id: number) => void;
+  selectedGenreId: number;
+};
+
+export function SideBar({ onClickButton, selectedGenreId }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
